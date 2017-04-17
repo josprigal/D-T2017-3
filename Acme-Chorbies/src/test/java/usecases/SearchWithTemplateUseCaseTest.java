2f52aa58595cb069fb43af5eac66a624c3bb6de5
Adri <adrigomezse@gmail.com>
2017-04-17 19:45:51 +0200

    Test nivel C 50% completado

---
+++ Acme-Chorbies/src/test/java/usecases/SearchWithTemplateUseCaseTest.java
@@ -0,0 +1,93 @@
+
+package usecases;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import services.ActorService;
+import services.ChorbiService;
+import services.CreditCardService;
+import services.SearchTemplateService;
+import utilities.AbstractTest;
+import domain.Chorbi;
+
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class SearchWithTemplateUseCaseTest extends AbstractTest {
+
+	@Autowired
+	ActorService			actorService;
+
+	@Autowired
+	ChorbiService			chorbiService;
+
+	@Autowired
+	SearchTemplateService	searchTemplateService;
+
+	@Autowired
+	CreditCardService		creditCardService;
+
+	@Autowired
+	private SessionFactory	sessionFactory;
+
+
+	/*
+	 * An actor who is authenticated as a chorbi must be able to:
+	 * 
+	 * Change his or her search template.
+	 */
+
+	@Test
+	public void testSearch() {
+		//Buscar con su template
+		super.authenticate("chorbi");
+		final Chorbi chorbi = this.chorbiService.findByPrincipal();
+		Assert.notNull(chorbi);
+
+		Assert.notNull(chorbi.getCreditCard());
+		Assert.isTrue(this.creditCardService.isValid(chorbi.getCreditCard()));
+		final Session session = this.sessionFactory.openSession();
+		Assert.notNull(this.searchTemplateService.search(session));
+
+		super.unauthenticate();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNoAuthenticated() {
+		//Buscar sin estar autentificado
+		super.authenticate(null);
+		final Chorbi chorbi = this.chorbiService.findByPrincipal();
+		Assert.notNull(chorbi);
+
+		Assert.notNull(chorbi.getCreditCard());
+		Assert.isTrue(this.creditCardService.isValid(chorbi.getCreditCard()));
+		Assert.notNull(this.searchTemplateService.search());
+
+		super.unauthenticate();
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testNoCreditCard() {
+		//Buscar con su template pero sin tarjeta
+		super.authenticate("chorbi");
+		Chorbi chorbi = this.chorbiService.findByPrincipal();
+		Assert.notNull(chorbi);
+		chorbi.setCreditCard(null);
+		this.chorbiService.save(chorbi);
+		chorbi = this.chorbiService.findByPrincipal();
+		Assert.notNull(chorbi.getCreditCard());
+		Assert.isTrue(this.creditCardService.isValid(chorbi.getCreditCard()));
+		Assert.notNull(this.searchTemplateService.search());
+
+		super.unauthenticate();
+	}
+}
